7e28413f35de27e8f2bc43edb87e7723a8882cf2
Ebsiy Anslem <anselmebsiy59@gmail.com>
2026-03-20 08:41:52 +0100

    Update app message to test GitOps

--- services/frontend/app.py
+++ services/frontend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Frontend service is running"
+ return "Welcome to the EKS GitOps Platform Frontend!"    
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
